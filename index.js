a9bcbcd2d3298c6708a74bbda7dbc80d71910eaa
TienDoan <nguyentiendoanhn09@gmail.com>
2022-03-27 16:55:18 +0700

    update code

---
+++ index.js
@@ -0,0 +1,8 @@
+a = [
+    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
+    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
+    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
+    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
+    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
+];
+console.table(a)
