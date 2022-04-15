1066c6be5ca8ed439dcfeaf24df6b43529a6fabb
doannt <doannt@unicloud.com.vn>
2022-04-15 10:38:08 +0700

    update to work1

--- README.md
+++
@@ -1,5 +0,0 @@
-# htmltest
-thử pull về xem thế nào 
-chưa pull về được
-thử pull vè 1 lần nữa
-và thế là được rồi 
--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-a = [
-    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
-    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
-    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
-    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
-    { name: "nguyễn tiến đoàn", age: 18, city: "hà nội" },
-];
-console.table(a)
+
+var school = {schoolName:"MIT", schoolAccredited: true, schoolLocation:"Massachusetts"};
+
+
+test = [
+{ name: "A", age: 20, status: true },
+{ name: "B", age: 21, status: true },
+{ name: "C", age: 22, status: true }]
+
+test = {name:'D', age: 23, status: false};
+test['name']= "E"
+demo = [
+    { quocgia: 'VN', id: 1 },
+    { quocgia: 'USA', id: 2 },
+    { quocgia: 'KD', id: 3 },
+    { quocgia: 'KD', id: 4 },
+    { quocgia: 'AC', id: 5 },
+    { quocgia: 'DF', id: 6 },
+]
+demo1 = [
+    'VN',
+    'USA',
+    'KD',
+    'KD',
+    'AC',
+    'DF',
+]
+
+
+ //copy toàn bộ mảng và dán nên từ vị trí thứ 1
+
+
+ //every() kiểm tra tất cả các phần tử trong mảng có thỏa mãn điều kiện đề không
+
+ //fill() biến các phần tử trong mảng thành các giá trị giống nhau
+ 
+ //filter() được sử dụng để lọc các phần tử trong mảng theo một điều kiện mà bạn đặt ra. Phương thức này sẽ trả về một mảng mới chứa các phần tử thỏa mãn điều kiện.
+
+ //find() được sử dụng khi bạn muốn tìm phần tử mảng đầu tiên thỏa mãn điều kiện mà bạn đặt ra.
+
+// findIndex() được sử dụng khi bạn muốn tìm vị trí của phần tử mảng đầu tiên thỏa mãn điều kiện bạn đặt
+
+// forEach() được sử dụng khi bạn muốn duyệt các phần tử mảng.
+
+// from() được sử dụng khi bạn muốn tạo ra một mảng mới từ một đối tượng dạng mảng.
+
+//includes() được sử dụng để kiểm tra một phần tử có xuất hiện trong mảng hay không. Phương thức này trả về true nếu phần tử mảng có xuất hiện, trả về false nếu phần tử mảng không xuất hiện.
+
